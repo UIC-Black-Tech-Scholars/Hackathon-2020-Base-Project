6cfcbe58f52943e28e51250efa0c96014e889cb8
Lucas Oskorep <lucas.oskorep@gmail.com>
2020-02-24 14:18:13 -0600

    Adding in project base

---
+++ Assets/Scripts/DataIO/GameData.cs
@@ -0,0 +1,17 @@
+﻿[System.Serializable]
+public class GameData
+{
+    public int score;
+    public string name;
+    public float timePlayed;
+
+
+    public GameData(int scoreInt, string nameStr, float timePlayedF)
+    {
+        score = scoreInt;
+        name = nameStr;
+        timePlayed = timePlayedF;
+
+        
+    }
+}
---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPosition : MonoBehaviour
+{
+    public bool HideOnStartup = true;
+    void Start()
+    {
+        if (HideOnStartup) gameObject.SetActive(false);
+        transform.localPosition = new Vector3(0, 0, transform.localPosition.z);
+    }
+}
