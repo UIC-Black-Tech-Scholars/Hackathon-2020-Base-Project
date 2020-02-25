0dd97a3c77193e86c795eb59d29d46fa6ca2fbfb
Lucas Oskorep <lucas.oskorep@gmail.com>
2020-02-24 18:59:18 -0600

    adding in classwork

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject MainMain;
+    public GameObject SettingsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onSettingButtonClicked()
+    {
+        Debug.Log("You have pressed the button");
+        MainMain.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SettingsManager : MonoBehaviour
+{
+    public GameObject MainMain;
+    public GameObject SettingsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onBackButonClicked()
+    {
+        Debug.Log("You have pressed the button");
+        MainMain.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+
+    public void onDropDownChanged(Dropdown change)
+    {
+        Debug.Log("New Value : " + change.value);
+        Debug.Log("New Value : " + change.name);
+
+    }
+}
